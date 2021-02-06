d3d2648c1c26d05ce10b12ab50aff8e6fac23932
megha <meghagupta190801@gmail.com>
2021-02-06 18:03:46 +0530

    06/02/2021

--- app/src/main/java/com/example/affirmations/child_fragments/PolicyFragment.kt
+++ app/src/main/java/com/example/affirmations/child_fragments/PolicyFragment.kt
@@ -10,11 +10,6 @@ import com.example.affirmations.R
 
 class PolicyFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
